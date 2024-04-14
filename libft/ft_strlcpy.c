6dda7ebebe3c956d7a7f4c111b7da987eb1dabcd
EnriqueSGZ <speaker_88@hotmail.com>
2024-04-14 20:50:40 +0200

    subida domingo

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,36 +6,35 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:57:59 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/14 16:51:45 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/04/14 19:31:35 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*char	ft_strlcpy(char *dst, char *src)
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int    i;
+	size_t  i;
     
     i = 0;
-    while (dst[i] != '\0')
+    while (src[i] != '\0' && i < dstsize - 1)
     {
         dst[i] = src[i];
         i++;
 	}
     dst[i] = '\0';
-	return (dst);
-}*/
+	return (i);
+}
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    char src[] = "123";
-    char dst[10] = "67890";
-
-    printf("Cadena destino antes de strlcpy: %s\n", dst);
+int main(void)
+{
+    char    src[] = "abdesdf";
+    char    dst[] = "67890"; 
+    size_t  dstsize = sizeof(dst);
     
-    size_t copied = strlcpy(dst, src, sizeof(dst));
-
-    printf("Cadena destino después de strlcpy: %s\n", dst);
-    printf("Tamaño de la cadena copiada: %zu\n", copied);
+    ft_strlcpy(dst, src, dstsize);
+    printf("Tamaño: %zu\n", dstsize);
+    printf("Cadena copiada: %s\n", dst);
 
     return 0;
 }
