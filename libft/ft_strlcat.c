f1944fe428b1597d98a996908e64196d35ae0a5c
EnriqueSGZ <166418672+EnriqueSGZ@users.noreply.github.com>
2024-04-17 17:21:57 +0000

    borrado error

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 19:27:58 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/15 13:14:38 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/17 17:19:05 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t	dstlen;
-    size_t	srclen;
-    size_t	i;
+	size_t	dstlen;
+	size_t	srclen;
+	size_t	i;
 	size_t	spaceleft;
 	
 	dstlen = 0;
@@ -25,38 +25,38 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	i = 0;
  
 	while (dst[dstlen] != '\0')
-    {
-        dstlen++;
-    }
-    while (src[srclen] != '\0')
-    {
-        srclen++;
-    }
-    if (size > dstlen)
 	{
-    spaceleft = size - dstlen - 1;
+		dstlen++;
+	}
+	while (src[srclen] != '\0')
+	{
+		srclen++;
+	}
+	if (size > dstlen)
+	{
+	spaceleft = size - dstlen - 1;
 	}
 	else
 	{
-    spaceleft = 0;
+	spaceleft = 0;
 	}
 	while (i < spaceleft && src[i] != '\0')
 	{
-        dst[dstlen + i] = src[i];
-        i++;
-    }
+		dst[dstlen + i] = src[i];
+		i++;
+	}
 	dst[dstlen + i] = '\0';
-    return (dstlen + srclen);
+	return (dstlen + srclen);
 }
 
 int main(void) 
 {
-    char dst[20] = "Pedro";
-    char src[] = " Picapiedra";
-    size_t size = sizeof(dst);
-    size_t newlen = ft_strlcat(dst, src, size);
+	char dst[20] = "Pedro";
+	char src[] = " Picapiedra";
+	size_t size = sizeof(dst);
+	size_t newlen = ft_strlcat(dst, src, size);
 	
-    printf("Concatena string: %s\n", dst);
-    printf("nuevo tamaño: %zu\n", newlen);
-    return (0);
+	printf("Concatena string: %s\n", dst);
+	printf("nuevo tamaño: %zu\n", newlen);
+	return (0);
 }
