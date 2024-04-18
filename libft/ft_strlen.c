46982fa5dde0c3314e991a3b250d21dce38cc53e
EnriqueSGZ <speaker_88@hotmail.com>
2024-04-18 15:27:06 +0200

    update

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:10:49 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/15 13:19:50 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/16 18:01:07 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,41 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:31:39 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/16 16:31:39 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/04/16 18:53:12 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (s[i] != c)
+	{
+		if (s[i] == c)
+		{
+			return ((char *)s);
+		}
+		i--;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	str[] = "Hola Mundo";
+	char	*p;
+
+	p = ft_strrchr (str, 'M');
+	printf ("El string seria: %s\n", p);
+	printf ("La direccion de memoria: %p\n", &p);
+	printf ("La posicion del caracter es: %d\n", (int)(p - str));
+	return (0);
+}
