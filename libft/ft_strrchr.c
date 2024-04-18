6a877a5f75f419e6de7678fd4c6618dc4b150676
EnriqueSGZ <speaker_88@hotmail.com>
2024-04-18 15:50:10 +0200

    strrchr terminado

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:31:39 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/16 18:53:12 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:49:34 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ char	*ft_strrchr(const char *s, int c)
 	{
 		i++;
 	}
-	while (s[i] != c)
+	while (i >= 0)
 	{
 		if (s[i] == c)
 		{
-			return ((char *)s);
+			return ((char *)&s[i]);
 		}
 		i--;
 	}
 	return (0);
 }
-
+/*
 int	main(void)
 {
 	char	str[] = "Hola Mundo";
@@ -43,4 +43,4 @@ int	main(void)
 	printf ("La direccion de memoria: %p\n", &p);
 	printf ("La posicion del caracter es: %d\n", (int)(p - str));
 	return (0);
-}
+}*/
