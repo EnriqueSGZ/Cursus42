57a6cd71860a75502ca0e023c49642f1ab2d76e9
EnriqueSGZ <speaker_88@hotmail.com>
2024-04-22 18:06:00 +0200

    atoi comentado main

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/22 17:46:46 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/22 18:05:35 by ensanche         ###   ########.fr       */
+/*   Created: 2024/04/22 18:04:39 by ensanche          #+#    #+#             */
+/*   Updated: 2024/04/22 18:04:39 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_atoi(const char *str)
-{
-	int	result;
-	int	i;
-
-	result = 0;
-	i = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = (result * 10 + (str[i] - '0'));
-		i++;
-	}
-	return (result);
-}
-/*
-int	main(void)
-{
-	char	*str = "12354";
-
-	printf("%d\n", ft_atoi(str));
-	return (0);
-}*/
