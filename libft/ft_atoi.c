7d760c685adf6934f049a75a4ff5a36f8c6aae1c
EnriqueSGZ <speaker_88@hotmail.com>
2024-04-22 18:03:40 +0200

    subida ft_atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 17:46:46 by ensanche          #+#    #+#             */
+/*   Updated: 2024/04/22 17:58:56 by ensanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	i;
+
+	result = 0;
+	i = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10 + (str[i] - '0'));
+		i++;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	char	*str = "12354";
+
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
