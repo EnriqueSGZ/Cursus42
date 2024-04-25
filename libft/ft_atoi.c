f0997156dce8c3f5027096167c827569252ef6c7
EnriqueSGZ <166418672+EnriqueSGZ@users.noreply.github.com>
2024-04-25 11:25:38 +0000

    makefile test

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:46:46 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/24 19:26:22 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/04/25 09:39:05 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	ft_atoi(const char *str)
 /*
 int	main(void)
 {
-	char	*str = "12354";
+	char	*str = "   +-+-+12354";
 
 	printf("%d\n", ft_atoi(str));
 	return (0);
