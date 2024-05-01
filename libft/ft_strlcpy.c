ac912214d5a0b8697809d3b8dd0f38394b411e68
EnriqueSGZ <speaker_88@hotmail.com>
2024-05-01 16:54:59 +0200

    strlcpy arreglado

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:57:59 by ensanche          #+#    #+#             */
-/*   Updated: 2024/04/25 12:22:24 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/05/01 16:53:35 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,24 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	srclen;
 
-	srclen = 0;
 	i = 0;
-	// Si el tamaño del búfer de destino es 0, devuelve la longitud de src
-	if (dstsize == 0)
+	if (dstsize > 0)
 	{
-        ft_strlen(src);
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
 	}
-    return (i);
-	// Copiamos hasta dstsize - 1 caracteres desde src a dst
-    while (src[i] != '\0' && i < dstsize - 1) 
+	if (i < dstsize)
 	{
-        dst[i] = src[i];
-        i++;
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	// Calculamos longitud de src
-	while (src[srclen] != '\0')
-	{
-		srclen++;
-	}
-	return (srclen);
+	return (ft_strlen(src));
 }
-
 /*
-int main(void)
+int	main(void)
 {
 	char    src[] = "abdesdf";
 	char    dst[] = "12345678"; 
