5337a47efd771b9517ff560f26b2b3b5f6a2dad2
EnriqueSGZ <166418672+EnriqueSGZ@users.noreply.github.com>
2024-05-07 17:26:13 +0000

    calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:42:24 by ensanche          #+#    #+#             */
-/*   Updated: 2024/05/07 16:53:35 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:25:56 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	*ft_calloc(size_t nmemb, size_t size);
-
-int main()
+void	*ft_calloc(size_t number, size_t size)
 {
+	void	*ptr;
+
+	ptr = malloc(number *size);
+	if (ptr == NULL)
+	{
+		return (ptr);
+	}
+	ft_bzero(ptr, number *size);
+	return (ptr);
+}
+
+int main(void)
+{
+	int	*array = (int *)ft_calloc(3, sizeof(int));
 	
+    *array = 10; *(array + 1) = 20; *(array + 2) = 30;
+    printf("%d %d %d\n", *array, *(array + 1), *(array + 2));
+    free(array);
 	return (0);
 }
