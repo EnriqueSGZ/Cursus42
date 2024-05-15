325b23a66963d691f2c82943b505bcd2990c5a06
EnriqueSGZ <speaker_88@hotmail.com>
2024-05-15 14:30:55 +0200

    subida ejercicios

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,41 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/13 11:55:38 by ensanche          #+#    #+#             */
-/*   Updated: 2024/05/15 13:52:02 by ensanche         ###   ########.fr       */
+/*   Created: 2024/05/15 13:40:23 by ensanche          #+#    #+#             */
+/*   Updated: 2024/05/15 14:29:34 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_strdup(const char *s)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	int		i;
+	char			*ptr;
+	unsigned int	i;
 
-	i = ft_strlen(s);
-	ptr = malloc(sizeof(char) * (i + 1));
-	if (ptr == NULL)
+	ptr = malloc(len);
+	i = 0;
+	if (s++)
 	{
-		return (NULL);
+		while (i < len)
+		{
+			ptr[i] = s[start + i];
+			i++;
+		}
+		return (ptr);
 	}
-	ft_memcpy(ptr, s, i);
-	ptr[i] = '\0';
-	return (ptr);
+	ptr[i] = '\0'
+	else
+		return (NULL);
 }
-/*
+
 int	main(void)
 {
-	char	*src = "hello";
-	char	*mine = NULL;
-	char	*original = NULL;
-	
-	mine = ft_strdup(src);
-	original = ft_strdup(src);
-	printf("%s\n%s\n", mine, original);
+	char const		*s = "Enrique";
+	unsigned int	start = 4;
+	size_t			len = 3;
+
+	printf("%s\n", ft_substr(s, start, len));
+	//free(s);
 	return (0);
-}*/
+}
