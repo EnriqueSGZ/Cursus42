0e62e9e52fefa0a4af126ba529d5efd26c52be6a
EnriqueSGZ <speaker_88@hotmail.com>
2024-06-10 17:55:52 +0200

    subida lstmap

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,10 +6,30 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 19:11:00 by ensanche          #+#    #+#             */
-/*   Updated: 2024/06/05 19:20:10 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:54:51 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*aux;
+
+	aux = NULL;
+	if (!f || !lst || !del)
+		return (NULL);
+	while (lst != NULL)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&aux, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&aux, new);
+		lst = lst->next;
+	}
+	return (aux);
+}
