296b4143090e5e41079405258798a2cac92f4a20
EnriqueSGZ <speaker_88@hotmail.com>
2024-06-10 17:16:43 +0200

    lst last y lst size update

--- libft/ft_lstlast_bonus.c
+++ libft/ft_lstlast_bonus.c
@@ -6,10 +6,21 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 19:08:35 by ensanche          #+#    #+#             */
-/*   Updated: 2024/06/05 19:19:52 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:16:08 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+	{
+		return (NULL);
+	}
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -6,10 +6,23 @@
 /*   By: ensanche <ensanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 19:08:08 by ensanche          #+#    #+#             */
-/*   Updated: 2024/06/05 19:18:07 by ensanche         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:08:52 by ensanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_lstsize(t_list *lst);
+int	ft_lstsize(t_list *lst)
+{
+	int	count;
+
+	if (!lst)
+		return (0);
+	count = 1;
+	while (lst->next != NULL)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
